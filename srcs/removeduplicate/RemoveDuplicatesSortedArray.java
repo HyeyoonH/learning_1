5ef71bece8223e6bc5c64d55d1057fc07ca52ebc
HyeyoonH <157762845+HyeyoonH@users.noreply.github.com>
2024-04-19 02:32:19 +0000

    another update

---
+++ srcs/removeduplicate/RemoveDuplicatesSortedArray.java
@@ -0,0 +1,35 @@
+// interface SolutionInterface{
+//     // public void solutionMethodVoid();
+//     // public int solutionMethodInt();
+//     public int solutionMethodIntFromIntArray(int[] nums);
+//     // public int[] solutionIntArray();
+
+// }
+
+
+// class SolutionMethod implements SolutionInterface {
+//     public int solutionMethodIntFromIntArray(int[] nums){
+//         int result = 0;
+
+//         return result; 
+//     }
+
+// }
+
+
+class Solution {
+    public int solutionMethodIntFromIntArray(int[] nums){
+        int result = 0;
+
+        return result; 
+    }
+
+}
+
+
+
+public class main{
+    public static void main(String[] args){
+        Solution solution = new Solution();
+    }
+}
