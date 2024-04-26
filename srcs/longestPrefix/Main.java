20554dbcdee21bbca0c7d5a19f329fe4d78fbb76
HyeyoonH <157762845+HyeyoonH@users.noreply.github.com>
2024-04-26 23:09:49 +0000

    commiting longest common prefix

--- srcs/longestPrefix/Main.java
+++ srcs/longestPrefix/Main.java
@@ -1,30 +1,38 @@
+import java.util.*;
+
 class Solution{
         
+    public int minLen(String[] strs){
+        int minLen = strs[0].length();
+        for(String s: strs){
+            minLen = Math.min(s.length(), minLen);
+        }
+        return minLen;
+    }
     public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 1) return strs[0];
-
-        int minStrLength = strs[0].length();
-        for(int i=1; i< strs.length; i++){
-            if(strs[i].length() < minStrLength){
-                minStrLength = strs[i].length();
-            }
-        }
-    
         StringBuffer sb = new StringBuffer();
-        for(int j=0; j < minStrLength; j++){
-            char c = strs[0].charAt(j) ; 
-            boolean same = true;
-            for(int k=1; k < strs.length; k++){
-                if(c != strs[k].charAt(j)){
-                    same = false;
-                    break;
-                }
-            }
-            if (!same) break;
-            sb.append(c);
-        }
+        int minLen = minLen(strs);
+        if(minLen == 0 || strs == null) return "";
+        if(strs.length ==1) return strs[0];
 
-    return sb.toString();
+        boolean characterMatch = false;
+        
+        Arrays.sort(strs);
+         for(int position=0; position < minLen; position++){
+           char c = strs[0].charAt(position);
+             for(int i=1; i < strs.length; i++){
+               if(c != strs[i].charAt(position)){
+                   return String.valueOf(sb);
+               } else {
+                   if(i == strs.length-1){
+                       sb = sb.append(c);
+                   }
+               }
+               
+            }
+        }
+        // return sb.toString(); toString() also works.
+        return String.valueOf(sb);
     }
 
 }
@@ -32,6 +40,12 @@ class Solution{
 
 public class Main {
     public static void main(String[] args){
+        
+        Solution solution = new Solution();
+        String[] input1 = {"flower","flow","flight"};
+        String[] input2 = new String[]{"dog","racecar","car"};
+
+        System.out.println(solution.longestCommonPrefix(input1));
 
     }
 }
